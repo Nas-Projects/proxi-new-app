7ddbefb32541c31330867aff2ba7ade1afda81c5
Nas <nassersanou23@gmail.com>
2024-09-28 16:25:22 +0200

    rm -rf reset

--- src/components/propertiesComponents/OtherProperties.jsx
+++ src/components/propertiesComponents/OtherProperties.jsx
@@ -1,7 +1,8 @@
 import Link from "next/link";
+import BlurImage from "../blur-image";
 
 export default function OtherProperties({}) {
-    return <div className="mb-44 main-container w-container">
+    return <div className="mb-44 pb-64 main-container w-container">
     <div className="house-button-wrap">
       <h4 className="no-margin ">Other properties</h4>
       <Link href="/properties" className="cta w-button bg-custom-gradient">
@@ -12,12 +13,12 @@ export default function OtherProperties({}) {
     <div className="w-dyn-list">
       <div role="list" className="houe-grid w-dyn-items">
         <div role="listitem" className="collection-item w-dyn-item">
-          <a
+          <Link
             style={{
               backgroundImage:
                 'url("https://cdn.prod.website-files.com/63c93770199def226e7204f3/63c93770199def25cc72052d_property-thumnail%203-min.jpg")'
             }}
-            href="/properties/solhjulet-236"
+            href="/properties/f2f75817-3106-4818-bdbb-b8cbce180c85"
             className="house-card w-inline-block"
           >
             <div className="house-content">
@@ -52,19 +53,19 @@ export default function OtherProperties({}) {
               </div>
             </div>
             <div className="overlay house" />
-          </a>
+          </Link>
         </div>
         <div role="listitem" className="collection-item w-dyn-item">
-          <a
+          <Link
             style={{
               backgroundImage:
-                'url("https://cdn.prod.website-files.com/63c93770199def226e7204f3/63c93770199def243a7204f9_property-thumnail%201-min.jpg")'
+                'url("/95251c1ee75b928ea8b4c2fce6bb11fe724495c8-1644x1065.webp")'
             }}
-            href="/properties/dragonveien-1"
+            href="/properties/f2f75817-3106-4818-bdbb-b8cbce180c85"
             className="house-card w-inline-block"
           >
             <div className="house-content">
-              <div className="house-title">Dragonveien 1</div>
+              <div className="house-title">627 2nd Avenue</div>
               <div className="house-card-divider" />
               <div className="house-detail-wrap">
                 <div className="detail-block white-block">
@@ -76,7 +77,7 @@ export default function OtherProperties({}) {
                       className="image"
                     />
                   </div>
-                  <p className="paragraph-small">Tromsø</p>
+                  <p className="paragraph-small">New York, NY</p>
                 </div>
                 <div className="detail-block white-block">
                   <div className="icon paragraph-small">
@@ -88,27 +89,27 @@ export default function OtherProperties({}) {
                     />
                   </div>
                   <div className="wrap-detail-value">
-                    <p className="paragraph-small">120</p>
-                    <p className="paragraph-small">m2</p>
+                    <p className="paragraph-small">1198</p>
+                    <p className="paragraph-small">ft2</p>
                   </div>
                 </div>
               </div>
             </div>
             <div className="overlay house" />
-          </a>
+          </Link>
         </div>
         <div role="listitem" className="collection-item w-dyn-item">
           <a
             style={{
               backgroundImage:
-                'url("https://cdn.prod.website-files.com/63c93770199def226e7204f3/63c93770199defb61072050b_property-thumnail%202-min.jpg")'
+                'url("/b435fa2d60b2741f5662128b0843b1e8e7295689-3000x4000.webp")'
             }}
-            href="/properties/storgata-15"
+            href="/properties/properties/e9e604ac-5d65-4e14-a444-9c8af98229c1"
             aria-current="page"
             className="house-card w-inline-block w--current"
           >
             <div className="house-content">
-              <div className="house-title">Storgata 15</div>
+              <div className="house-title">110 Lexington Avenue - Restaurant</div>
               <div className="house-card-divider" />
               <div className="house-detail-wrap">
                 <div className="detail-block white-block">
@@ -120,20 +121,22 @@ export default function OtherProperties({}) {
                       className="image"
                     />
                   </div>
-                  <p className="paragraph-small">Oslo</p>
+                  <p className="paragraph-small">New York NY</p>
                 </div>
                 <div className="detail-block white-block">
                   <div className="icon paragraph-small">
-                    <img
-                      src="https://cdn.prod.website-files.com/63c93770199def60f97204e0/63c93770199defda7c72052e_door_back.svg"
+                  <BlurImage
+                      src="/b435fa2d60b2741f5662128b0843b1e8e7295689-3000x4000.webp"
                       loading="lazy"
                       alt=""
                       className="image"
+                      height={200}
+                      width={200}
                     />
                   </div>
                   <div className="wrap-detail-value">
-                    <p className="paragraph-small">210</p>
-                    <p className="paragraph-small">m2</p>
+                    <p className="paragraph-small">1300</p>
+                    <p className="paragraph-small">ft2</p>
                   </div>
                 </div>
               </div>
@@ -141,7 +144,7 @@ export default function OtherProperties({}) {
             <div className="overlay house" />
           </a>
         </div>
-        <div role="listitem" className="collection-item w-dyn-item">
+        {/* <div role="listitem" className="collection-item w-dyn-item">
           <a
             style={{
               backgroundImage:
@@ -183,8 +186,8 @@ export default function OtherProperties({}) {
             </div>
             <div className="overlay house" />
           </a>
-        </div>
-      </div>
+        </div>*/}
+      </div> 
     </div>
   </div>
   
