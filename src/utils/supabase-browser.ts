509f3b022f482a6d5467d32901b1e9ccaa2c062b
Nas <nassersanou23@gmail.com>
2024-09-29 05:36:33 +0200

    revoves deprecated deps

--- src/utils/supabase-browser.ts
+++
@@ -1,6 +0,0 @@
-// utils/supabase-browser.ts
-import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs';
-
-const supabase = createBrowserSupabaseClient();
-
-export default supabase;
