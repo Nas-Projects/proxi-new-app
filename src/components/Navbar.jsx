b76ebe2815bbe653bb5fccdac2b0ab9d18324c5a
Nas <nassersanou23@gmail.com>
2024-09-30 07:49:15 +0200

    Adds Home

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -25,6 +25,7 @@ import  NavbarUserAvatarComponent  from './NavbarUserAvatarComponent'
 //     'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
 // }
 const navigation = [
+  { name: 'Home', href: '/', current: true },
   { name: 'Properties', href: '/properties', current: true },
   { name: 'Sales', href: '/properties/search-results?location=&propertyType=sale"', current: false },
   { name: 'Rentals', href: '/properties/search-results?location=&propertyType=rentals', current: false },
@@ -341,7 +342,7 @@ console.log("errorMessage", errorMessage);
               href={item.href}
               aria-current={item.current ? 'page' : undefined}
               className={classNames(
-                item.current ? 'bg-pink-200 text-black !text-xl' : 'mainText hover:bg-pink-300 hover:text-black dark:hover:text-white',
+                item.current ? 'bg-pink-100/80 text-black !text-xl' : 'mainText hover:bg-pink-300 hover:text-black dark:hover:text-white',
                 'block rounded-md px-3 py-2 text-lg font-medium ',
               )}
             >
