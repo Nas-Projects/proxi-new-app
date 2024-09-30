5e3707f8a3342700af497bbf73ae73b4371dfd71
Nas <nassersanou23@gmail.com>
2024-09-30 19:48:11 +0200

    lsyout shift test

--- src/components/propertiesComponents/PropertySearchForm.jsx
+++ src/components/propertiesComponents/PropertySearchForm.jsx
@@ -1,6 +1,12 @@
 'use client';
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
+import Link from 'next/link';
+import { Checkbox, Field, Label,Input } from '@headlessui/react';
+
+import clsx from 'clsx';
+import { CheckIcon } from '@heroicons/react/20/solid';
+import { Button } from '../button';
 
 const PropertySearchForm = () => {
   const [location, setLocation] = useState('');
@@ -20,50 +26,71 @@ const PropertySearchForm = () => {
   };
 
   return (
-    <form
-      onSubmit={handleSubmit}
-      className='mt-3 mx-auto max-w-2xl w-full flex flex-col md:flex-row items-center'
-    >
-      <div className='w-full md:w-3/5 md:pr-2 mb-4 md:mb-0'>
-        <label htmlFor='location' className='sr-only'>
-          Location
-        </label>
-        <input
-          type='text'
-          id='location'
-          placeholder='Enter Keywords or Location'
-          className='w-full px-4 py-3 rounded-lg !bg-white text-gray-800 focus:outline-none focus:ring ring-1 ring-pink-300  focus:ring-blue-500 hover:shadow-lg '
-          value={location}
-          onChange={(e) => setLocation(e.target.value)}
-        />
+        <div className="justify-center items-center w-full">
+          <form  onSubmit={handleSubmit} action="#" method="POST"
+           className="md:inline-flex  sm:p-7 
+                  sm:p-11 mt-3 
+                  max-w-7xl w-full 
+                  md::inline-flex 
+                  flex-col 
+                  md:flex-row 
+                  items-center">
+            
+              <Field className="mt-8 space-y-3">
+                {/* <Label className="text-sm/5 font-medium">Email</Label> */}
+                <Input
+                  // required
+                  autoFocus
+                  value={location}
+                  type="text"
+                  name="location"
+                  className={clsx(
+                    'block w-full rounded-lg border border-transparent shadow ring-1 ring-black/10 px-4 py-3 ',
+                    'px-[calc(theme(spacing.2)-1px)] py-[calc(theme(spacing[1.5])-1px)] text-base/6 sm:text-sm/6',
+                    'data-[focus]:outline data-[focus]:outline-2 data-[focus]:-outline-offset-1 data-[focus]:outline-black',
+                  )}
+                  placeholder='Enter Keywords or Location'
+                  //       className='w-full px-4 py-3 rounded-lg !bg-white text-gray-800 focus:outline-none focus:ring ring-1 ring-pink-300  focus:ring-blue-500 hover:shadow-lg '
+              
+                      onChange={(e) => setLocation(e.target.value)}
+                />
+              </Field>
+              <Field className="mt-8 space-y-3">
+                <div className='w-full  md:pl-2'>
+              <label htmlFor='property-type' className='sr-only'>
+                Property Type
+              </label>
+              <select
+                id='property-type'
+                className='w-full px-4 py-3 rounded-lg !bg-white text-gray-800 focus:outline-none focus:ring ring-1 ring-pink-300  focus:ring-blue-500 hover:shadow-lg '
+                value={propertyType}
+                onChange={(e) => setPropertyType(e.target.value)}
+              >
+                <option value='All'>All</option>
+                <option value='Retail'>Retail</option>
+                <option value='Office'>Office</option>
+                <option value='Investment Sale'>Investment Sale</option>
+                <option value='Rental'>Rental</option>
+                <option value='Residential'>Residential</option>
+                <option value='Commercial'>Commercial</option>
+              </select>
+            </div>
+              </Field>
+            
+              <div className="mt-8">
+                <button
+              type='submit'
+              className='md:ml-4 mt-4 md:mt-0 w-full md:w-auto px-6 py-3 
+            rounded-lg bg-black text-white bg-custom-gradient hover:bg-black  focus:outline-none focus:ring focus:ring-blue-500'
+            >
+              Search
+          </button>
+              
+              </div>
+            </form>
       </div>
-      <div className='w-full md:w-2/5 md:pl-2'>
-        <label htmlFor='property-type' className='sr-only'>
-          Property Type
-        </label>
-        <select
-          id='property-type'
-          className='w-full px-4 py-3 rounded-lg !bg-white text-gray-800 focus:outline-none focus:ring ring-1 ring-pink-300  focus:ring-blue-500 hover:shadow-lg '
-          value={propertyType}
-          onChange={(e) => setPropertyType(e.target.value)}
-        >
-          <option value='All'>All</option>
-          <option value='Retail'>Retail</option>
-          <option value='Office'>Office</option>
-          <option value='Investment Sale'>Investment Sale</option>
-          <option value='Rental'>Rental</option>
-          <option value='Residential'>Residential</option>
-          <option value='Commercial'>Commercial</option>
-        </select>
-      </div>
-      <button
-        type='submit'
-        className='md:ml-4 mt-4 md:mt-0 w-full md:w-auto px-6 py-3 
-        rounded-lg bg-black text-white bg-custom-gradient hover:bg-black  focus:outline-none focus:ring focus:ring-blue-500'
-      >
-        Search
-      </button>
-    </form>
+    
+   
   );
 };
 
--- src/components/propertiesComponents/SearchResultsComponent .jsx
+++ src/components/propertiesComponents/SearchResultsComponent .jsx
@@ -4,15 +4,21 @@ import SearchResultsCSR from './SearchResultsCSR.jsx'; // Your separated CSR com
 
 export default function SearchResultsComponent() {
   return (
-    <>
-      <section className='bg-custom-gradient px-4 py-12 lg:py-12'>
-        <div className='max-w-7xl mx-auto px-4 flex flex-col items-start sm:px-6 lg:px-8'>
-          <PropertySearchForm />
+    <div className="overflow-hidden bg-gray-50">
+        {/* <GradientBackground /> */}
+        <div className="isolate flex min-h-dvh items-center justify-center p-6 lg:p-8">
+          <div className="w-full max-w-full justify-center content-center  items-center
+           rounded-xl bg-white shadow-md ring-1 ring-black/5">
+            <section className='bg-custom-gradient px-4 py-12 lg:py-12'>
+              <div className='max-w-7xl mx-auto px-4 flex flex-col items-start sm:px-6 lg:px-8'>
+                <PropertySearchForm />
+              </div>
+            </section>
+            <Suspense fallback={<div>Loading search results...</div>}>
+              <SearchResultsCSR />
+            </Suspense>
+            </div>
         </div>
-      </section>
-      <Suspense fallback={<div>Loading search results...</div>}>
-        <SearchResultsCSR />
-      </Suspense>
-    </>
+      </div>
   );
 }
