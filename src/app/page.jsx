9bc78676ef6eca4d26249d5ee92d7d1aaff1f53d
Nas <nassersanou23@gmail.com>
2024-10-01 05:00:29 +0200

    WIP 0

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -15,6 +15,7 @@ import SectionContact from '@/components/propertiesComponents/SectionContact';
 import SectionBlockChain from '@/components/propertiesComponents/SectionBlockChain';
 import SectionJoin from '@/components/propertiesComponents/SectionJoin';
 import SectionTestimonial from '@/components/propertiesComponents/SectionTestimonial';
+import SectionServices from '@/components/propertiesComponents/SectionServices';
 import SectionOurTeams from '@/components/propertiesComponents/SectionOurTeams';
 // import FeaturedListings from '@/components/propertiesComponents/FeaturedListings';
 import ProperTiesForSale from '@/components/propertiesComponents/ProperTiesForSale';
@@ -55,150 +56,18 @@ async function Home({}){
         <Hero   //  heroDetails={heroDetails}
           />
           {/* <div className="mx-auto lg:px-[10%] bg-transparent  absolute absolute top-[89vh] lg:top-[68vh] lg:mx-auto md:top-[70vh] xl:top-[67vh]"></div> */}
-          <div className="mt-44 relative px-0.5 lg:mt-[-24em] xl:lg:mt-[-0vh] mx-auto lg:px-[10%] bg-transparent relative relative ">
-          <SearchButtons />
-          </div>
-          <ProperTiesForSale properties={properties}/>
-          <AboutProxy/>
+           <div className="mt-44 relative px-0.5 lg:mt-[-24em] xl:lg:mt-[-0vh] mx-auto lg:px-[10%] bg-transparent relative relative ">
+           <SearchButtons />
+           </div>
+           <ProperTiesForSale properties={properties}/>
+           <AboutProxy/>
            <PropertiesListSectionUI properties={properties}/>
           {/* <FeaturedListings /> */}
-          <SectionBlockChain/>
-          <OurValues/> 
+           <SectionBlockChain/>
+           <OurValues/> 
           {/* <OurFocus/> */}
-          <SectionTestimonial/>
-          <div className="section buy-service">
-           <div className="large w-container">
-            <div
-            data-w-id="b613acd8-a7a8-18c7-bb65-e5559fefd061"
-            className="buy-service-wrap"
-            style={{ opacity: 1 }}>
-            <div className="buy-service-left bg-custom-gradient">
-              <div className="block ">
-                <h2 className='!text-white'>Proxy landlord & brokers&apos; service</h2>
-                <div className="space-16" />
-                <p className="paragraph-big">
-                Proxy Landlord & Brokers&apos; Service connects property owners with qualified tenants and expert brokers, streamlining the rental process. Our dedicated team ensures a seamless experience, maximizing returns while minimizing hassle.
-                </p>
-              </div>
-              <div className="buy-service-separator">
-                <p className="separator-text">
-                  What&apos;s included? Absolutely everything.
-                </p>
-                <div className="separator-line" />
-              </div>
-              <div className="checklist-wrap">
-                <div className="checklist">
-                  <div className="checklist-item">
-                    <div className="checklist-icon">
-                      <img
-                        src="https://cdn.prod.website-files.com/63c93770199def60f97204e0/63c93770199def2025720557_Check.svg"
-                        loading="lazy"
-                        alt=""
-                        className="image"
-                      />
-                    </div>
-                    <p>We arrange the valuation report</p>
-                  </div>
-                  <div className="checklist-item">
-                    <div className="checklist-icon">
-                      <img
-                        src="https://cdn.prod.website-files.com/63c93770199def60f97204e0/63c93770199def2025720557_Check.svg"
-                        loading="lazy"
-                        alt=""
-                        className="image"
-                      />
-                    </div>
-                    <p>We check your financial situation</p>
-                  </div>
-                  <div className="checklist-item">
-                    <div className="checklist-icon">
-                      <img
-                        src="https://cdn.prod.website-files.com/63c93770199def60f97204e0/63c93770199def2025720557_Check.svg"
-                        loading="lazy"
-                        alt=""
-                        className="image"
-                      />
-                    </div>
-                    <p>We take care of the negotiations</p>
-                  </div>
-                  <div className="checklist-item">
-                    <div className="checklist-icon">
-                      <img
-                        src="https://cdn.prod.website-files.com/63c93770199def60f97204e0/63c93770199def2025720557_Check.svg"
-                        loading="lazy"
-                        alt=""
-                        className="image"
-                      />
-                    </div>
-                    <p>We provide the building inspection</p>
-                  </div>
-                </div>
-                <div className="checklist">
-                  <div className="checklist-item">
-                    <div className="checklist-icon">
-                      <img
-                        src="https://cdn.prod.website-files.com/63c93770199def60f97204e0/63c93770199def2025720557_Check.svg"
-                        loading="lazy"
-                        alt=""
-                        className="image"
-                      />
-                    </div>
-                    <p>We discuss your living requirements</p>
-                  </div>
-                  <div className="checklist-item">
-                    <div className="checklist-icon">
-                      <img
-                        src="https://cdn.prod.website-files.com/63c93770199def60f97204e0/63c93770199def2025720557_Check.svg"
-                        loading="lazy"
-                        alt=""
-                        className="image"
-                      />
-                    </div>
-                    <p>We review the purchase agreement</p>
-                  </div>
-                  <div className="checklist-item">
-                    <div className="checklist-icon">
-                      <img
-                        src="https://cdn.prod.website-files.com/63c93770199def60f97204e0/63c93770199def2025720557_Check.svg"
-                        loading="lazy"
-                        alt=""
-                        className="image"
-                      />
-                    </div>
-                    <p>We help you with the opening proposal</p>
-                  </div>
-                  <div className="checklist-item">
-                    <div className="checklist-icon">
-                      <img
-                        src="https://cdn.prod.website-files.com/63c93770199def60f97204e0/63c93770199def2025720557_Check.svg"
-                        loading="lazy"
-                        alt=""
-                        className="image"
-                      />
-                    </div>
-                    <p>You&apos;ll get free Proxy Realestate merch</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="buy-service-right !bg-[var(---agedBlack)]">
-              <div className="block">
-                <p>Starting from</p>
-                <div className="space-10" />
-                <h1 className="no-margin">$27</h1>
-              </div>
-              <div className="block ">
-                <a href="#" className="cta large w-button bg-custom-gradient">
-                  Speak with us - It&apos;s free
-                </a>
-                <div className="space-10" />
-                <p className='py-4'>No commitment · Set up in minutes</p>
-              </div>
-            </div>
-            </div>
-           </div>
-         </div>
-
+            <SectionTestimonial/>
+            <SectionServices/>
             <SectionOurTeams />
             <SectionJoin/>
             <SectionContact/>
---
+++ src/components/propertiesComponents/SectionServices.jsx
@@ -0,0 +1,146 @@
+export default function SectionTestimonial() {
+    return (
+      <div className="bg-white/60 py-16 sm:py-24">
+        <div className="mx-auto max-w-7xl xl:max-w-[1400px]">
+          <div className="relative overflow-hiddenpy-20 shadow-xl sm:rounded-3xl sm:py-24">
+           {/* ----- */}
+           <div className="section buy-service">
+           <div className="large w-container xl:!max-w-[1400px]">
+            <div
+            data-w-id="b613acd8-a7a8-18c7-bb65-e5559fefd061"
+            className="buy-service-wrap"
+            style={{ opacity: 1 }}>
+            <div className="buy-service-left bg-custom-gradient ring-1 ring-black border-1 shadow-2xl">
+              <div className="block ">
+                <h2 className='!text-white'>Proxy landlord & brokers&apos; service</h2>
+                <div className="space-16" />
+                <p className="paragraph-big">
+                Proxy Landlord & Brokers&apos; Service connects property owners with qualified tenants and expert brokers, streamlining the rental process. Our dedicated team ensures a seamless experience, maximizing returns while minimizing hassle.
+                </p>
+              </div>
+              <div className="buy-service-separator">
+                <p className="separator-text">
+                  What&apos;s included? Absolutely everything.
+                </p>
+                <div className="separator-line" />
+              </div>
+              <div className="checklist-wrap">
+                <div className="checklist">
+                  <div className="checklist-item">
+                    <div className="checklist-icon">
+                      <img
+                        src="https://cdn.prod.website-files.com/63c93770199def60f97204e0/63c93770199def2025720557_Check.svg"
+                        loading="lazy"
+                        alt=""
+                        className="image"
+                      />
+                    </div>
+                    <p>We arrange the valuation report</p>
+                  </div>
+                  <div className="checklist-item">
+                    <div className="checklist-icon">
+                      <img
+                        src="https://cdn.prod.website-files.com/63c93770199def60f97204e0/63c93770199def2025720557_Check.svg"
+                        loading="lazy"
+                        alt=""
+                        className="image"
+                      />
+                    </div>
+                    <p>We check your financial situation</p>
+                  </div>
+                  <div className="checklist-item">
+                    <div className="checklist-icon">
+                      <img
+                        src="https://cdn.prod.website-files.com/63c93770199def60f97204e0/63c93770199def2025720557_Check.svg"
+                        loading="lazy"
+                        alt=""
+                        className="image"
+                      />
+                    </div>
+                    <p>We take care of the negotiations</p>
+                  </div>
+                  <div className="checklist-item">
+                    <div className="checklist-icon">
+                      <img
+                        src="https://cdn.prod.website-files.com/63c93770199def60f97204e0/63c93770199def2025720557_Check.svg"
+                        loading="lazy"
+                        alt=""
+                        className="image"
+                      />
+                    </div>
+                    <p>We provide the building inspection</p>
+                  </div>
+                </div>
+                <div className="checklist">
+                  <div className="checklist-item">
+                    <div className="checklist-icon">
+                      <img
+                        src="https://cdn.prod.website-files.com/63c93770199def60f97204e0/63c93770199def2025720557_Check.svg"
+                        loading="lazy"
+                        alt=""
+                        className="image"
+                      />
+                    </div>
+                    <p>We discuss your living requirements</p>
+                  </div>
+                  <div className="checklist-item">
+                    <div className="checklist-icon">
+                      <img
+                        src="https://cdn.prod.website-files.com/63c93770199def60f97204e0/63c93770199def2025720557_Check.svg"
+                        loading="lazy"
+                        alt=""
+                        className="image"
+                      />
+                    </div>
+                    <p>We review the purchase agreement</p>
+                  </div>
+                  <div className="checklist-item">
+                    <div className="checklist-icon">
+                      <img
+                        src="https://cdn.prod.website-files.com/63c93770199def60f97204e0/63c93770199def2025720557_Check.svg"
+                        loading="lazy"
+                        alt=""
+                        className="image"
+                      />
+                    </div>
+                    <p>We help you with the opening proposal</p>
+                  </div>
+                  <div className="checklist-item">
+                    <div className="checklist-icon">
+                      <img
+                        src="https://cdn.prod.website-files.com/63c93770199def60f97204e0/63c93770199def2025720557_Check.svg"
+                        loading="lazy"
+                        alt=""
+                        className="image"
+                      />
+                    </div>
+                    <p>You&apos;ll get free Proxy Realestate merch</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="buy-service-right !bg-[var(---agedBlack)]">
+              <div className="block">
+                <p>Starting from</p>
+                <div className="space-10" />
+                <h1 className="no-margin">$27</h1>
+              </div>
+              <div className="block ">
+                <a href="#" className="cta large w-button bg-custom-gradient">
+                  Speak with us - It&apos;s free
+                </a>
+                <div className="space-10" />
+                <p className='py-4'>No commitment · Set up in minutes</p>
+              </div>
+            </div>
+            </div>
+           </div>
+         </div>
+
+         {/* ------- */}
+      </div>
+     </div>
+    </div>
+   )
+  
+  }
